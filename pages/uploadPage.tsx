edfbf18e860bb99b6c7c96334193f9660a544ab4
wmzspace <sc21m2w@leeds.ac.uk>
2023-03-03 09:17:50 +0800

    modified uploadPage.tsx

--- pages/uploadPage.tsx
+++ pages/uploadPage.tsx
@@ -10,13 +10,34 @@ import {
   StyleSheet,
   View,
 } from 'react-native';
-import {Button, Searchbar, Text} from 'react-native-paper';
+import {Button, Searchbar, Text, TextInput} from 'react-native-paper';
 import ScreenWrapper from '../@components/ScreenWrapper';
 import {globalVal, userInfo} from '../values/global';
 import {getNftImgs} from '../apis/api';
+import styles from "../styles";
 
 export const UploadScreen = () => {
-  // const [url, setUrl] = React.useState('');
+  const [url, setUrl] = React.useState('');
+  const [nftName, setNftName] = React.useState('');
+  const [nftDescription, setNftDescription] = React.useState('');
+  const [owner, setOwner] = React.useState('');
+  const [fee, setFee] = React.useState(0);
+
+  let uploadInfo = {
+    url: '',
+    nftName: '',
+    nftDescription: '',
+    owner: '',
+    fee: 0,
+  };
+
+  const setUploadInfo = uploadInfo => {
+    setUrl(uploadInfo.url);
+    setNftName(uploadInfo.nftName);
+    setNftDescription(uploadInfo.nftDescription);
+    setOwner(uploadInfo.owner);
+    setFee(uploadInfo.fee);
+  };
 
   return (
     <View>
@@ -51,6 +72,35 @@ export const UploadScreen = () => {
           });
         }}>
         Test
+      </Button>
+      <Button
+        onPress={() => {
+          fetch('http:/' + serverIPP + '/upload ', {
+            method: 'POST',
+            mode: 'cors',
+            body: JSON.stringify({
+              headers: 'testHeader',
+              body: 'testbody',
+            }),
+            headers: {
+              ACCEPCT: 'application/json',
+              ContentType: 'application/www-json-strencoded',
+            },
+          })
+            .then(res => {
+              if (res.ok) {
+                res.text().then(resData => {
+                  console.log(JSON.stringify(resData));
+                });
+              } else {
+                Alert.alert('请求失败', 'Res error');
+              }
+            })
+            .catch(err => {
+              console.log(err.message);
+            });
+        }}>
+        Show upload
       </Button>
     </View>
   );
