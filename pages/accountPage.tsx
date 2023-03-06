709c07625d7d18906d93706cbeb99781c2bf52fd
wmzspace <sc21m2w@leeds.ac.uk>
2023-03-06 14:51:53 +0800

    modified

--- pages/accountPage.tsx
+++ pages/accountPage.tsx
@@ -10,15 +10,34 @@ import {
   StyleSheet,
   View,
 } from 'react-native';
-import {Searchbar, Text} from 'react-native-paper';
+import {Searchbar, Text, List, Button} from 'react-native-paper';
 import ScreenWrapper from '../@components/ScreenWrapper';
 import {globalVal, userInfo} from '../values/global';
 import {getNftImgs} from '../apis/api';
 
 export const AccountScreen = () => {
+  const [expanded, setExpanded] = React.useState(false);
   return (
     <View>
       <Text>!</Text>
+      <List.Section title="List1">
+        <List.Accordion title="uncontrolled list" expanded={!expanded}>
+          <List.Item title="Item1-1" />
+          <List.Item title="Item1-2" />
+        </List.Accordion>
+
+        <List.Accordion title="controlled list" expanded={expanded}>
+          <List.Item title="Item1-1" />
+          <List.Item title="Item1-2" />
+        </List.Accordion>
+      </List.Section>
+      <Button
+        onPress={() => {
+          setExpanded(!expanded);
+        }}>
+        {' '}
+        Switch Expanded
+      </Button>
     </View>
   );
 };
